1eb3addaefe2692b78b8f9f92dfb2dcbb4dee13b
wisnuwardana182 <wisnuwardana182@outlook.com>
2021-04-20 16:11:58 +0800

    Latihan Stateful Widget

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //Method
+  int number =1;
+  void tekantombol() {
+    setState(() {
+      number = number +1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan StatefulWidget"),),
+        body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(number.toString()),
+            MaterialButton(
+            color: Colors.blue[100],
+            height: 50,
+            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(5.0)),
+            onPressed: tekantombol,
+            child: Text("Tambah"),
+          ),
+    ],
+    ),
+      ),
+    ),
+    );
+  }
+}
